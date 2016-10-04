d404724c951fc67b887da350d3a05d9333fce568
Rachel Sharp <ras.beckman@gmail.com>
2016-10-04 16:34:52 +0100

    Added a placeholder About page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
+  get '/about' => 'application#about'
+
   resources :aliens, only: [:index, :show]
   resources :expansions, only: :index
 end
